e5918cd9201e3c527f41d69aa576e880f405e46d
Prince Verma <16836599+vprince001@users.noreply.github.com>
2018-10-24 15:11:10 +0530

    Add asserts for generateFilledRectangle function

--- test/patterns_util_test.js
+++ test/patterns_util_test.js
@@ -2,6 +2,18 @@ const assert = require("assert");
 const util_lib = require("../src/util_lib.js");
 
 const {repeatCharacter} = util_lib;
+const {generateFilledRectangle} = util_lib;
+
+//-----------------repeatCharacter-----------------//
 assert.equal(repeatCharacter("*", 0),"");
 assert.equal(repeatCharacter("*", 1),"*");
 assert.equal(repeatCharacter("-", 2),"--");
+
+//-------------generateFilledRectangle-------------//
+let generateFilledRectangle_2_5 = "*****" + "\n";
+generateFilledRectangle_2_5 +=    "*****";
+assert.equal(generateFilledRectangle(2,5), generateFilledRectangle_2_5);
+
+let generateFilledRectangle_2_2 = "**" + "\n";
+generateFilledRectangle_2_2 +=    "**";
+assert.equal(generateFilledRectangle(2,2), generateFilledRectangle_2_2);
