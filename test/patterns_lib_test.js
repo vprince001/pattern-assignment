fe62d27024195628c4bc41dade7d31da6bce35b4
Prince Verma <16836599+vprince001@users.noreply.github.com>
2018-10-24 15:26:26 +0530

    Add asserts for generateEmptyRectangle function

--- test/patterns_lib_test.js
+++ test/patterns_lib_test.js
@@ -13,7 +13,6 @@ filledRectangle_2_2 +=    "**";
 assert.equal(generateRectangle('filled',2,2),filledRectangle_2_2);
 
 //-------------(Empty Rectangle Tests)-------------//
-
 let emptyRectangle_5_8 = "********" + "\n";
 emptyRectangle_5_8 +=    "*      *" + "\n";
 emptyRectangle_5_8 +=    "*      *" + "\n";
@@ -36,7 +35,6 @@ emptyRectangle_12_5 +=    "*****";
 assert.equal(generateRectangle("empty",12,5),emptyRectangle_12_5);
 
 //-----------(Alternate Rectangle Tests)-----------//
-
 let alternateRectangle_2_8 = "********" + "\n";
 alternateRectangle_2_8 +=    "--------";
 assert.equal(generateRectangle('alternate',2,8),alternateRectangle_2_8);
@@ -47,7 +45,6 @@ alternateRectangle_5_2 +=    "**" + "\n";
 alternateRectangle_5_2 +=    "--" + "\n";
 alternateRectangle_5_2 +=    "**";
 assert.equal(generateRectangle('alternate',5,2),alternateRectangle_5_2);
-
 
 //================(TRIANGLE TESTS)=================//
 const {generateTriangle} = require("../src/patterns_lib.js");
@@ -66,7 +63,6 @@ leftTriangle_7 += "*******";
 assert.equal(generateTriangle("left",7),leftTriangle_7);
 
 //---------(Right Alignment Triangle Test)---------//
-
 let rightTriangle_2 = " *" + "\n";
 rightTriangle_2 +=    "**";
 assert.equal(generateTriangle("right",2),rightTriangle_2);
@@ -79,12 +75,10 @@ rightTriangle_6 +=    " *****" + "\n";
 rightTriangle_6 +=    "******";
 assert.equal(generateTriangle("right",6),rightTriangle_6);
 
-
 //=================(DIAMOND TESTS)=================//
 const {generateDiamond} = require("../src/patterns_lib.js");
 
 //--------------(Filled Diamond Test)--------------//
-
 let filledDiamond_3 = " *"  +"\n";
 filledDiamond_3 +=    "***" +"\n";
 filledDiamond_3 +=    " *";
@@ -98,7 +92,6 @@ filledDiamond_5 +=    "  *";
 assert.equal(generateDiamond("filled",5),filledDiamond_5);
 
 //--------------(Hollow Diamond Test)--------------//
-
 let hollowDiamond_3 = " *"  + "\n";
 hollowDiamond_3 +=    "* *" + "\n";
 hollowDiamond_3 +=    " *";
@@ -112,7 +105,6 @@ hollowDiamond_5 +=    "  *";
 assert.equal(generateDiamond("hollow",5),hollowDiamond_5);
 
 //--------------(Angled Diamond Test)--------------//
-
 let angledDiamond_3 = " *"  + "\n";
 angledDiamond_3 +=    "* *" + "\n";
 angledDiamond_3 +=    " *";
@@ -124,4 +116,3 @@ angledDiamond_5 +=    "*   *" + "\n";
 angledDiamond_5 +=    " \\ /" + "\n";
 angledDiamond_5 +=    "  *";
 assert.equal(generateDiamond("angled",5),angledDiamond_5);
-
--- test/patterns_util_test.js
+++ test/patterns_util_test.js
@@ -3,6 +3,7 @@ const util_lib = require("../src/util_lib.js");
 
 const {repeatCharacter} = util_lib;
 const {generateFilledRectangle} = util_lib;
+const {generateEmptyRectangle} = util_lib;
 
 //-----------------repeatCharacter-----------------//
 assert.equal(repeatCharacter("*", 0),"");
@@ -17,3 +18,25 @@ assert.equal(generateFilledRectangle(2,5), generateFilledRectangle_2_5);
 let generateFilledRectangle_2_2 = "**" + "\n";
 generateFilledRectangle_2_2 +=    "**";
 assert.equal(generateFilledRectangle(2,2), generateFilledRectangle_2_2);
+
+//--------------generateEmptyRectangle-------------//
+let generateEmptyRectangle_5_8 = "********" + "\n";
+generateEmptyRectangle_5_8 +=    "*      *" + "\n";
+generateEmptyRectangle_5_8 +=    "*      *" + "\n";
+generateEmptyRectangle_5_8 +=    "*      *" + "\n";
+generateEmptyRectangle_5_8 +=    "********";
+assert.equal(generateEmptyRectangle(5,8), generateEmptyRectangle_5_8);
+
+let generateEmptyRectangle_12_5 = "*****" + "\n";
+generateEmptyRectangle_12_5 +=    "*   *" + "\n";
+generateEmptyRectangle_12_5 +=    "*   *" + "\n";
+generateEmptyRectangle_12_5 +=    "*   *" + "\n";
+generateEmptyRectangle_12_5 +=    "*   *" + "\n";
+generateEmptyRectangle_12_5 +=    "*   *" + "\n";
+generateEmptyRectangle_12_5 +=    "*   *" + "\n";
+generateEmptyRectangle_12_5 +=    "*   *" + "\n";
+generateEmptyRectangle_12_5 +=    "*   *" + "\n";
+generateEmptyRectangle_12_5 +=    "*   *" + "\n";
+generateEmptyRectangle_12_5 +=    "*   *" + "\n";
+generateEmptyRectangle_12_5 +=    "*****";
+assert.equal(generateEmptyRectangle(12,5), generateEmptyRectangle_12_5);
