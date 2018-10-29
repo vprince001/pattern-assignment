f33ea194422dbce1c5db3cc982b1f7ee753ccd83
Prince Verma <16836599+vprince001@users.noreply.github.com>
2018-10-29 13:05:42 +0530

    Manage file system and add new scripts

--- script/run_app_test.sh
+++ script/run_app_test.sh
@@ -1,4 +1,3 @@
-set -e
 ./run_test.sh ./create_rectangle.js ./test/create_rectangle_input ./test/create_rectangle_output
 ./run_test.sh ./create_triangle.js ./test/create_triangle_input ./test/create_triangle_output
 ./run_test.sh ./create_diamond.js ./test/create_diamond_input ./test/create_diamond_output
---
+++ script/run_unit_test.sh
@@ -0,0 +1,1 @@
+node ./test/patterns_lib_test.js
